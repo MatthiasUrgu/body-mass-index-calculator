61f93beeec759c4c3f8b60e59cddc89c113f7224
Matthias.Urgu <matthias.urgu@gmail.com>
2023-07-25 11:59:24 +0200

    desinstall typescript devDep

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './App.module.scss'
-import Header from "./components/header/header"
+import Header from "components/header/header"
 
 function App() {
   return (
