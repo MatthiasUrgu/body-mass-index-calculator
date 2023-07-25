9c53785f29bc424b667b2c4c5d1ac414633d1f77
Matthias.Urgu <matthias.urgu@gmail.com>
2023-07-25 11:14:47 +0200

    absolute path learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import s from './App.module.scss'
-import Header from './components/header/header'
+import Header from "./components/header/header"
 
 function App() {
   return (
     <div className={s.container}>
-      <Header/>
+      <Header />
     </div>
   );
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import s from './style.module.scss'
-function header() {
+
+
+function Header() {
 
     /* use useState to radioButton to change in same times the taille:'CM' to 'Inch'  weight:Kg to Ib  */
     return (
+        
         <div className={s.boxContainer}>
-            <div className={s.blueBox}></div>
-            <div className={s.textHeader}>
-                <h1>Body Mass Index Calculator</h1>
-                <p>Better understand your weight in relation to your height using our body mass index (BM) calculator. While is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
-            </div>
-            <div className={s.bmiCalculator}>
-                <h1>Enter your details below</h1>
-                <div className="flexRadio">
-                    <input type="radio" name="Metric" id="cm"  />
-                    <label>Metric</label>
-                    <input type="radio" name="imperial" id="inch" />
-                    <label>Imperial</label>
+           
+            <div className={s.headerContainer}>
+
+                <img src="" alt="" />
+                <div className={s.textHeader}>
+                    <h1>Body Mass Index Calculator</h1>
+                    <p>Better understand your weight in relation to your height using our body mass index (BM) calculator. While is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
                 </div>
+                <div className={s.bmiCalculator}>
+                    <h1>Enter your details below</h1>
+                    <div className="flexRadio">
+                        <input type="radio" name="Metric" id="cm"  />
+                        <label>Metric</label>
+                        <input type="radio" name="imperial" id="inch" />
+                        <label>Imperial</label>
+                    </div>
+                </div>
+                <div>feofjoefj</div>
+                
             </div>
+            <div className={s.blueBox}></div>
         </div>
     );
 }
 
-export default header;
+export default Header;
