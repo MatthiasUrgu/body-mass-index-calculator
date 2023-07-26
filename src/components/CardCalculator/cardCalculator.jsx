18b5cdc75f89f4e581cb1039c0405658389e0b4b
Matthias.Urgu <matthias.urgu@gmail.com>
2023-07-26 21:15:14 +0200

    Calculator
    IMC Done plus que BMI

--- src/components/CardCalculator/cardCalculator.jsx
+++ src/components/CardCalculator/cardCalculator.jsx
@@ -3,27 +3,51 @@ import s from './style.module.scss'
 
 function CardCalculator({result,resultText}) {
 
-    const [isHeight,setHeight] = useState(true)
+    const [isMetric,setMetric] = useState("metric")
+    const [Height,setHeight] = useState(0)
+    const [Weight,setWeight] = useState(0)
+
 
     function toggleMetric(e){
-        setHeight(e.target.value === 'on')
-        
-        console.log(isHeight);
+        setMetric(!isMetric)
+       console.log(isMetric);
+    }
+    function handleHeight(e){
+        setHeight(e.target.value)
+        console.log(setHeight);
+    }
+    function handleWeight(e){
+        setWeight(e.target.value)
     }
 
+    function calculateBMI(){
+        if(isMetric === "metric"){
+            if(Height && Weight){
+                var IMC = Weight / (Height*Height)
+                return IMC.toFixed(1)
 
+        }
+    }
+        else if(isMetric === "imperial"){
+            if(Height && Weight){
+                var BMI = (Weight / (Height*Height)) * 703
+                return BMI.toFixed(1)
+            }
+        }
+    }
 
+  
     return (
         <div className={s.bmiCalculator}>
                     <h1>Enter your details below</h1>
                     <div className={`${s.flexRadio} ${s.marginTop}`}>
                         <div className={s.metric}>
-                            <input onChange={toggleMetric} type="radio" name="Metric" id="height" value="on" checked={isHeight}/>
+                            <input onChange={toggleMetric} type="radio" name="Metric"  value="metric" checked={isMetric} />
                             
                             <label>Metric</label>
                         </div>
                         <div className={s.imperial}>
-                            <input onChange={toggleMetric} type="radio" name="imperial" id="weight" value="off" checked={!isHeight}/>
+                            <input onChange={toggleMetric} type="radio" name="imperial" value="imperial" checked={!isMetric}/>
                             <label>Imperial</label>
                         </div>
                     </div>
@@ -31,23 +55,28 @@ function CardCalculator({result,resultText}) {
                     <div className={`${s.calculNumber} ${s.marginTop}`}>
                         <div className={s.heightNumber}>
                            <label className={s.labelHeight}>Height</label>
-                            <input className={s.inputHeight} type="number" />
+
+                            <input className={s.inputHeight} type="number" value={Height} onChange={handleHeight}/>
                             <span className={s.spanMetric}>
-                                {isHeight ? 'M' : 'inch'}
+                                {isMetric ? 'M' : 'inch'}
                             </span>
                         </div>
                         <div className={`${s.weightNumber} ${s.marginTop}`}>
                             <label className={s.labelHeight}>Weight</label>
-                            <input className={s.inputHeight}type="number" />
+
+                            <input className={s.inputHeight}type="number" value={Weight} onChange={handleWeight}/>
                             <span className={s.spanMetric}>
-                                {isHeight ? 'Kg' : 'Ibs'}
+                                {isMetric ? 'Kg' : 'Ibs'}
                             </span>
                         </div>
                     </div>
                     <div className={`${s.resultNumber} ${s.marginTop}`}>
                         <h3>Your BMI is...</h3>
-                        <span className={s.result}>{result}</span>
-                        <p>Your BMi suggest you're a healthy weight. Yiour ideal weight is between  {result} {isHeight ? 'Kg' : 'Ibs'}.</p>
+                        <span className={s.result}>
+                            {Height && Weight ? `${calculateBMI()}` : "Padawan tu es Zeubi"}
+                            
+                            </span>
+                        <p>Your BMi suggest you're a healthy weight. Yiour ideal weight is between  {result} {isMetric ? 'Kg' : 'Ibs'}.</p>
 
                     </div>
                 </div>
